e557dd6aeb45f5f050d56b3e2bb9c8fb265a94e9
remix4 <remix4@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-20 09:29:56 +0000

    First Version
    git-svn-id: https://plugins.svn.wordpress.org/jquery-slick-menu/trunk@362428 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ dcwp_jquery_slick_menu.php
@@ -0,0 +1,56 @@
+<?php
+/*
+		Plugin Name: jQuery Slick Menu
+		Plugin URI: http://www.designchemical.com/blog/index.php/wordpress-plugins/wordpress-plugin-jquery-slick-menu-widget/
+		Tags: jquery, flyout, menu, vertical, animated, css, navigation, widget, slider
+		Description: Creates a widget, which adds a sticky sliding menu from any Wordpress custom menu.
+		Author: Lee Chestnutt
+		Version: 1.0
+		Author URI: http://www.designchemical.com
+*/
+
+global $registered_skins;
+
+class dc_jqslickmenu {
+
+	function dc_jqslickmenu(){
+		global $registered_skins;
+	
+		if(!is_admin()){
+			// Header styles
+			add_action( 'wp_head', array('dc_jqslickmenu', 'header') );
+			// Scripts
+			wp_enqueue_script( 'jquery' );
+			wp_enqueue_script( 'dcjqslickmenu', dc_jqslickmenu::get_plugin_directory() . '/js/jquery.slick.menu.1.0.js', array('jquery') );
+		}
+		add_action( 'wp_footer', array('dc_jqslickmenu', 'footer') );
+		
+		$registered_skins = array();
+	}
+
+	function header(){
+		echo "\n\t<link rel=\"stylesheet\" type=\"text/css\" href=\"".dc_jqslickmenu::get_plugin_directory()."/css/dcslickmenu.css\" media=\"screen\">";
+	}
+	
+	function footer(){
+		//echo "\n\t";
+	}
+	
+	function options(){}
+
+	function get_plugin_directory(){
+		return WP_PLUGIN_URL . '/jquery-slick-menu';	
+	}
+
+};
+
+// Include the widget
+include_once('dcwp_jquery_slick_menu_widget.php');
+
+// Initialize the plugin.
+$dcjqslickmenu = new dc_jqslickmenu();
+
+// Register the widget
+add_action('widgets_init', create_function('', 'return register_widget("dc_jqslickmenu_widget");'));
+
+?>
